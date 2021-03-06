ccd177087f881be9a19573952a6fc41c011c508a
Josephine Bromley <bromleyjosephine@gmail.com>
2021-03-06 16:30:31 +0000

    added filter functionality

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -1,6 +1,14 @@
+require 'json'
+
 class GiftsController < ApplicationController
   def index
     @shortlist = Shortlist.find(params[:shortlist_id])
-    @gifts = Gift.all
+    @interests = @shortlist.interest.split("/")
+    @gifts = []
+    @interests.drop(1).each do |interest|
+      interest_list = JSON.parse(interest)
+      raise
+      @gifts << Gift.find_by_sql("SELECT * FROM gifts WHERE main_category = '#{interest_list}';")
+    end
   end
 end
