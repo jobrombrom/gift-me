8c225dc7ec1509852c3538ca2461d7676e6a2b5c
Josephine Bromley <bromleyjosephine@gmail.com>
2021-03-06 16:52:45 +0000

    small change to gift controller

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -3,12 +3,12 @@ require 'json'
 class GiftsController < ApplicationController
   def index
     @shortlist = Shortlist.find(params[:shortlist_id])
-    @interests = @shortlist.interest.split("/")
+    @interests = @shortlist.interest.split('"')
+    @interests.select! { |i| i =~ /[A-Z]/ }
+    raise
     @gifts = []
-    @interests.drop(1).each do |interest|
-      interest_list = JSON.parse(interest)
-      raise
-      @gifts << Gift.find_by_sql("SELECT * FROM gifts WHERE main_category = '#{interest_list}';")
+    @interests.each do |interest|
+      @gifts << Gift.find_by_sql("SELECT * FROM gifts WHERE main_category = '#{interest}';")
     end
   end
 end
