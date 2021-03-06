397ae5fab60af6b834c311e975b4a28d646feab5
Polly ONeill <polly.c.oneill@gmail.com>
2021-03-06 12:34:56 +0000

    basic card (image wont comply)

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2021_03_04_192857) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,14 +25,6 @@ ActiveRecord::Schema.define(version: 2021_03_04_192857) do
     t.string "price"
     t.string "main_category"
     t.string "sub_category"
-  end
-
-  create_table "recipients", force: :cascade do |t|
-    t.string "name"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_recipients_on_user_id"
   end
 
   create_table "session_gifts", force: :cascade do |t|
@@ -68,7 +60,6 @@ ActiveRecord::Schema.define(version: 2021_03_04_192857) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "recipients", "users"
   add_foreign_key "session_gifts", "gifts"
   add_foreign_key "session_gifts", "shortlists"
   add_foreign_key "shortlists", "users"
