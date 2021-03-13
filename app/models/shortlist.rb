6784f49b663d3307c5f3007325d0f18758b96d7d
Polly ONeill <polly.c.oneill@gmail.com>
2021-03-13 16:34:37 +0000

    home page

--- app/models/shortlist.rb
+++ app/models/shortlist.rb
@@ -2,22 +2,23 @@ class Shortlist < ApplicationRecord
   belongs_to :user
   has_many :session_gifts
   has_many :gifts, through: :session_gifts
+  validates :recipient_name, presence: :true
 
-  # validates :interest, inclusion: { in: [
-  #   "Pet Supplies",
-  #   "Clothing",
-  #   "Home & Living",
-  #   "Jewelry",
-  #   "Art & Collectibles",
-  #   "Bags & Purses",
-  #   "Craft Supplies & Tools",
-  #   "Electronics & Accessories",
-  #   "Accessories",
-  #   "Books, Movies & Music",
-  #   "Weddings",
-  #   "Shoes",
-  #   "Toys & Games",
-  #   "Bath & Beauty",
-  #   "Paper & Party Supplies"
-  # ] }
+  CATEGORIES = [
+    "Pet Supplies",
+    "Clothing",
+    "Home & Living",
+    "Jewelry",
+    "Art & Collectibles",
+    "Bags & Purses",
+    "Craft Supplies & Tools",
+    "Electronics & Accessories",
+    "Accessories",
+    "Books, Movies & Music",
+    "Weddings",
+    "Shoes",
+    "Toys & Games",
+    "Bath & Beauty",
+    "Paper & Party Supplies"
+    ]
 end
