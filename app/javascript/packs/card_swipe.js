b84dff73d3173127b8041d6d9108520bdab90f94
Polly ONeill <polly.c.oneill@gmail.com>
2021-03-18 18:52:29 +0000

    stacked look works need buttons

--- app/javascript/packs/card_swipe.js
+++ app/javascript/packs/card_swipe.js
@@ -6,21 +6,18 @@ var allCards = document.querySelectorAll('.gift-card');
 // is below just going to get the first one :/
 var nope = document.getElementById('nope');
 var love = document.getElementById('love');
+var allNopes = document.querySelectorAll('#nope');
+var allLoves = document.querySelectorAll('#love');
 
-console.log(giftContainer);
-console.log(allCards);
-console.log(nope);
-console.log(love);
+// console.log(allNopes);
+// console.log(allLoves);
 
 
 function initCards(card, index) {
   var newCards = document.querySelectorAll('.gift-card:not(.removed)');
 
-  newCards.forEach(function (card, index) {
-    console.log(card)
-    console.log(index)
+newCards.forEach(function (card, index) {
     // order of cards
-    // card.style.zIndex = allCards.length - index;
     card.style.zIndex = index
     let order = allCards.length - index - 1
     card.style.transform = 'scale(' + (20 - order) / 20 + ') translateY(-' + 30 * order + 'px)';    
@@ -28,42 +25,54 @@ function initCards(card, index) {
   });
   
   giftContainer.classList.add('loaded');
-}
+};
 
 initCards();
 
+// only works for last one because uses get element by id and we have multiple buttons
+// swiping
+function createButtonListener(love) {
+  return function (event) {
+    var cards = document.querySelectorAll('.gift-card:not(.removed)');
+    console.log(cards)
+    var moveOutWidth = document.body.clientWidth * 1.5;
 
+    if (!cards.length) return false;
 
+    // add removed class to card
+    var card = cards[0];
+    card.classList.add('removed');
+    // transition
+    if (love) {
+      card.style.transform = 'translate(' + moveOutWidth + 'px, -100px) rotate(-30deg)';
+    } else {
+      card.style.transform = 'translate(-' + moveOutWidth + 'px, -100px) rotate(30deg)';
+    }
 
+    initCards();
 
-// // swiping
-// function createButtonListener(love) {
-//   return function (event) {
-//     var cards = document.querySelectorAll('.gift-card:not(.removed)');
-//     console.log(cards)
-//     var moveOutWidth = document.body.clientWidth * 1.5;
-
-//     if (!cards.length) return false;
-
-//     var card = cards[0];
-//     // add removed class to card
-//     card.classList.add('removed');
-//     // transition
-//     if (love) {
-//       card.style.transform = 'translate(' + moveOutWidth + 'px, -100px) rotate(-30deg)';
-//     } else {
-//       card.style.transform = 'translate(-' + moveOutWidth + 'px, -100px) rotate(30deg)';
-//     }
-
-//     // initCards();
-
-//     event.preventDefault();
-//   };
-// }
+    event.preventDefault();
+  };
+}
   
-// var nopeListener = createButtonListener(false);
-// var loveListener = createButtonListener(true);
+var nopeListener = createButtonListener(false);
+var loveListener = createButtonListener(true);
 
-// nope.addEventListener('click', nopeListener);
-// love.addEventListener('click', loveListener);
+
+// function frontButton(nodeList) {
+//   let counter = 1
+//   var nope = allNopes[allNopes.length - counter];
+//   console.log(nope);
+//   counter += 1
+//   return nope;
+// };
+
+// let frontNope = frontButton(allNopes);
+
+
+
+nope.addEventListener('click', nopeListener);
+love.addEventListener('click', loveListener);
   
+nope.addEventListener('click', console.log(nope));
+love.addEventListener('click', console.log(love));
