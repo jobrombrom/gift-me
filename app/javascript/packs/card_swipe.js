85f936eacae345b67d8ecdc02e7cc756354d73ad
Polly ONeill <polly.c.oneill@gmail.com>
2021-03-18 19:42:34 +0000

    swiping javascript

--- app/javascript/packs/card_swipe.js
+++ app/javascript/packs/card_swipe.js
@@ -3,11 +3,9 @@
 var giftContainer = document.querySelector('.gift-display');
 // allCards = node list
 var allCards = document.querySelectorAll('.gift-card');
-// is below just going to get the first one :/
-var nope = document.getElementById('nope');
-var love = document.getElementById('love');
-var allNopes = document.querySelectorAll('#nope');
-var allLoves = document.querySelectorAll('#love');
+
+var allNopes = document.querySelectorAll('.nope');
+var allLoves = document.querySelectorAll('.love');
 
 // console.log(allNopes);
 // console.log(allLoves);
@@ -15,25 +13,62 @@ var allLoves = document.querySelectorAll('#love');
 
 function initCards(card, index) {
   var newCards = document.querySelectorAll('.gift-card:not(.removed)');
+  newCards = Array.from(newCards).reverse();
 
 newCards.forEach(function (card, index) {
     // order of cards
-    card.style.zIndex = index
-    let order = allCards.length - index - 1
-    card.style.transform = 'scale(' + (20 - order) / 20 + ') translateY(-' + 30 * order + 'px)';    
-    card.style.opacity = (10 - order) / 10;
+ 
+    card.style.zIndex = allCards.length - index;
+    card.style.transform = 'scale(' + (20 - index) / 20 + ') translateY(-' + 30 * index + 'px)';
+    card.style.opacity = (10 - index) / 10;
+    // card.style.zIndex = index
+
+    // let order = allCards.length - index - 1
+    // card.style.transform = 'scale(' + (20 - order) / 20 + ') translateY(-' + 30 * order + 'px)';    
+    // card.style.opacity = (10 - order) / 10;
   });
   
   giftContainer.classList.add('loaded');
+
+  var card = newCards[0];
+  var loveButton = card.closest('.gift-row').querySelector('.love')
+  var nopeButton = card.closest('.gift-row').querySelector('.nope')
+    
+
+  var nopeListener = createButtonListener(false);
+  var loveListener = createButtonListener(true);
+
+
+  nopeButton.addEventListener('click', nopeListener);
+  loveButton.addEventListener('click', loveListener);
 };
 
 initCards();
+
+// function frontButton(nodeList) {
+//   // get front card
+//   // get parent class 
+//   // get button 
+    // initCards();
+
+// };
+
+// let frontNope = frontButton(allNopes);
+
+
+
+
+
+
+
+
 
 // only works for last one because uses get element by id and we have multiple buttons
 // swiping
 function createButtonListener(love) {
   return function (event) {
     var cards = document.querySelectorAll('.gift-card:not(.removed)');
+    cards = Array.from(cards).reverse()
     console.log(cards)
     var moveOutWidth = document.body.clientWidth * 1.5;
 
@@ -54,25 +89,13 @@ function createButtonListener(love) {
     event.preventDefault();
   };
 }
+
+
   
-var nopeListener = createButtonListener(false);
-var loveListener = createButtonListener(true);
-
-
-// function frontButton(nodeList) {
-//   let counter = 1
-//   var nope = allNopes[allNopes.length - counter];
-//   console.log(nope);
-//   counter += 1
-//   return nope;
-// };
-
-// let frontNope = frontButton(allNopes);
 
 
 
-nope.addEventListener('click', nopeListener);
-love.addEventListener('click', loveListener);
-  
-nope.addEventListener('click', console.log(nope));
-love.addEventListener('click', console.log(love));
+
+
+// nope.addEventListener('click', console.log(nope));
+// love.addEventListener('click', console.log(love));
